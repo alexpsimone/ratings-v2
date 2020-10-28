19f7b487b63f2dd27ccf84636bb2ceda6dc4a249
alexpsimone <alex.p.simone@gmail.com>
2020-10-28 23:53:55 +0000

    added User Model

--- model.py
+++ model.py
@@ -5,7 +5,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# Replace this with your code!
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        primary_key = True,
+                        autoincrement = True)
+    email = db.Column(db.String, unique = True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+    
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
